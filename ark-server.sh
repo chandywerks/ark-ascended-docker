96f2b8385bac7b43c13530a4c3360651c4a43b81
chandywerks <chandwerker@pm.me>
2023-11-01 18:26:56 -0400

    Fix ShooterGame.log capture

--- ark-server.sh
+++ ark-server.sh
@@ -7,10 +7,7 @@ steamcmd +login anonymous +app_update 2430930 validate +quit
 SERVER_CMD="$PROTON run \"${ARK_PATH}/Binaries/Win64/ArkAscendedServer.exe\" \
   \"TheIsland_WP?listen?SessionName=${SESSION_NAME}?ServerAdminPassword=${ADMIN_PASSWORD}?Port=${GAME_PORT}?QueryPort=${QUERY_PORT}?MaxPlayers=${MAX_PLAYERS}?\""
 
-# Enable logs
-SERVER_CMD="$SERVER_CMD -servergamelog -servergamelogincludetribelogs"
-
-# Configure mods
+# Install mods
 if [ -n "$MODS" ]; then
   SERVER_CMD="$SERVER_CMD -automanagedmods -mods=$MODS"
 fi
@@ -20,5 +17,13 @@ if [ -n "$CMD_ARGS" ]; then
   SERVER_CMD="$SERVER_CMD $CMD_ARGS"
 fi
 
-eval $SERVER_CMD
+# Start the server
+eval $SERVER_CMD > /dev/null 2>&1 &
+SERVER_PID=$!
+
+# Capture logs
+tail -f "${ARK_PATH}/Saved/Logs/ShooterGame.log" &
+
+# Monitor server process
+wait $SERVER_PID
 exit $?
